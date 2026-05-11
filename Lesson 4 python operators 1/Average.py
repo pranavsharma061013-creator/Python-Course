eadd20fad566bc05eb4d675b171e8e896a0b0272
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-11 14:22:54 +0530

    initial commit

---
+++ Lesson 4 python operators 1/Average.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 15
+number3 = 23
+number4 = 36
+number5 = 42
+sum = number1 + number2 + number3 + number4 + number5
+print("The sum of the numbers is", sum)
+average = sum / 5
+print("Average of the numbers is", average)
