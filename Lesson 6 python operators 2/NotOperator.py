f37f4c433576398836134ff2444bc7c0ec8c52a1
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-13 14:34:36 +0530

    initial commit

---
+++ Lesson 6 python operators 2/NotOperator.py
@@ -0,0 +1,14 @@
+a = 5
+b = 8
+c = 8
+print(not(a == b))
+print(not(b == c))
+
+a = 4 
+b = 5
+if not((a == 1) == (b == 5)):
+    print("Hello")
+
+a = int(input("Enter a number:"))
+if not(a % 2 == 0):
+    print("The number is odd")
