54efa94d3b3558da4a12489be411d2033652b462
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-14 14:29:22 +0530

    initial commit

---
+++ Lesson 7 python operators 3/Identity.py
@@ -0,0 +1,18 @@
+x = 10
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+
+x = 5.5
+if (type(x) is int):
+    print("true")
+else:
+    print("false")
+          
+x = 20
+y = 20
+if (x is y):
+    print("same identity")
+else:
+    print("different identity")
