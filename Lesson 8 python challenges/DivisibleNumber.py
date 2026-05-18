6f19803345e95622cf9a62284e2b86c735390a08
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-18 14:35:44 +0530

    initial commit

---
+++ Lesson 8 python challenges/DivisibleNumber.py
@@ -0,0 +1,9 @@
+print("Enter a number:")
+num = int(input())
+print("Enter a divisor:")
+divisor = int(input())
+
+if num % divisor == 0:
+    print("The number is divisible by the divisor.")
+else:
+    print("The number is not divisible by the divisor.")
