30dd6ddbbe5459bd2315adf053e90ba1f7a919a1
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-18 14:25:47 +0530

    initial commit

---
+++ Lesson 8 python challenges/OperatorPrecedense.py
@@ -0,0 +1,14 @@
+a = 15
+b = 28
+c = 32
+d = 47
+e = 54
+z = (a + b)*c / d
+print(z)
+
+name = "alex"
+age = 0
+if name == "alex" or name == "john" and age > 2:
+    print("hello")
+else: 
+    print("bye")
