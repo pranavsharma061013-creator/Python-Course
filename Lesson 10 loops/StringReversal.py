a8802e2a73757b4f04cf12dee1e9b58ce5e4e939
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-20 14:29:37 +0530

    initial commit

---
+++ Lesson 10 loops/StringReversal.py
@@ -0,0 +1,8 @@
+string = input("Enter the string you want to reverse")
+
+string2 = ("")
+for i in string:
+    string2 = i + string2
+
+print("The original string =", string)
+print("The reversed string =", string2)
