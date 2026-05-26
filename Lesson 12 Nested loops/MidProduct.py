ae43b782fcf9ee33e02398dedab10a3f353f1cf4
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-26 14:16:19 +0530

    initial commit

---
+++ Lesson 12 Nested loops/MidProduct.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number:"))
+t = num
+numLen = 0
+while t > 0:
+    numLen = numLen+1
+    t = int(t/10)
+    if numLen >= 4:
+        numLen = int(numLen/2)
+        chk = 0
+        while num%10 
+        if chk == numLen:
+            midOne
---
+++ Lesson 13 patterns/RightAngledTriangle.py
@@ -0,0 +1,6 @@
+print("Half Pyramind Pattern of stars (*):")
+n = int(input("enter the number of rows:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
