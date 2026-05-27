70796398306eb1c7937796a20e0f8e14cd361d8f
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-05-27 14:57:34 +0530

    initial commit

---
+++ Python test 1/Python test.py
@@ -0,0 +1,12 @@
+print("Number Guessing Game")
+print(num = int(input("Enter a number")))
+
+print
+if range(1,31):
+    print("Ice cold")
+elif range(31,61):
+    print("cold")
+elif range(61,71):
+    print("hot")
+elif range("71,81"):
+    print("Very hot")
