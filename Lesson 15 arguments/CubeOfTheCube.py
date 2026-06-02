b22249e138a7ea2954ece3b427c0eaf40681b921
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-06-02 14:26:16 +0530

    initial commit

---
+++ Lesson 15 arguments/CubeOfTheCube.py
@@ -0,0 +1,11 @@
+def cube(number):
+    return number*number*number
+
+def by_three(number):
+    if number %3 == 0:
+        return cube(number)
+    else:
+        return False
+    
+print(by_three(17))
+print(by_three(12))
