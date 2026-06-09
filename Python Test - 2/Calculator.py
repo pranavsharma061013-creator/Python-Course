90d273a073808f04cd27c56bd529b87a9e2a9974
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-06-09 14:50:46 +0530

    initial commit

---
+++ Python Test - 2/Calculator.py
@@ -0,0 +1,15 @@
+print("CALCULATOR GAME")
+a = int(input("Enter a number:"))
+b = int(input("Enter a second number:"))
+option = int(input("Choose the operation needed:"))
+def addition_1(a,b):
+    return a + b 
+
+
+
+
+
+print("a. Addition")
+print("b. Subtraction")
+print("c. Multiplication")
+print("d. Division")
