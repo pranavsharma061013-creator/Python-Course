7ce294082059a1a7f443745df84866bcc19fc674
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-06-10 14:45:38 +0530

    initial commit

---
+++ Lesson 19 getting started with list/PlayWithLists.py
@@ -0,0 +1,16 @@
+L = [4,5,1,2,9,7,10,8]
+print("Original List:",L)
+count = 0
+for i in L:
+    count += i
+
+avg = count/len(L)
+
+print("sum = ",count)
+print("average = ",avg)
+
+L.sort()
+
+print("smallest element is:", L[0])
+print("Largest element is:", L[-1])
+
