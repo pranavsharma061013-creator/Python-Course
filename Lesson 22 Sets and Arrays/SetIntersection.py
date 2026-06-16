94867233b89a5bd7a7ac74ceef273d950c88d4bd
pranavsharma061013-creator <pranav.sharma061013@gmail.com>
2026-06-16 14:35:47 +0530

    initial commit

---
+++ Lesson 22 Sets and Arrays/SetIntersection.py
@@ -0,0 +1,10 @@
+setx = {"green","blue"}
+sety = {"blue", "yellow"}
+print("original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of tow said seats:")
+setz = setx.intersection(sety)
+print(setz)
+setz = setx.union(sety)
+print(setz)
